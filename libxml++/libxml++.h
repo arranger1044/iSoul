08069a72c868d14aeff5d1e73331fb672a6fcbf4
David King <davidk@openismus.com>
2010-04-16 14:26:50 +0200

    Minor documentation update
    * docs/index.html: Link to latest gnome.org resources.
    * libxml++/libxml++.h: Add minimal documentation for main page.

--- libxml++/libxml++.h
+++ libxml++/libxml++.h
@@ -7,6 +7,19 @@
 #ifndef __LIBXMLCPP_H
 #define __LIBXMLCPP_H
 
+/** @mainpage libxml++ Reference Manual
+ *
+ * @section description Description
+ *
+ * libxml++ is a C++ wrapper for the libxml2 XML parser library. libxml2 and
+ * glibmm are required. libxml++ presents a simple C++-like API that can
+ * achieve common tasks with less code.
+ *
+ * @section use Use
+ *
+ * To use libxml++ in your application, include one of the header files. A
+ * @c pkg-config file is provided to simplify compilation.
+ */
 #include <libxml++/exceptions/internal_error.h>
 #include <libxml++/exceptions/parse_error.h>
 #include <libxml++/parsers/domparser.h>
