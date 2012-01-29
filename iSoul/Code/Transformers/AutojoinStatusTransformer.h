70c0b3ebe911c836fe969fae96bc26bac73b4019
arranger1044 <arranger1044@aim.com>
2012-01-29 20:53:12 +0100

    Added a star icon to indicate that a chat room has been autojoined (any suggestion for the icon will be appreciated, still need more icons!)

---
+++ iSoul/Code/Transformers/AutojoinStatusTransformer.h
@@ -0,0 +1,14 @@
+//
+//  AutojoinStatusTransformer.h
+//  iSoul
+//
+//  Created by valerio on 29/01/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AutojoinStatusTransformer : NSValueTransformer {
+}
+
+@end
---
+++ iSoul/Code/Transformers/AutojoinStatusTransformer.m
@@ -0,0 +1,37 @@
+//
+//  AutojoinStatusTransformer.m
+//  iSoul
+//
+//  Created by valerio on 29/01/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "AutojoinStatusTransformer.h"
+
+@implementation AutojoinStatusTransformer
+
++ (Class)transformedValueClass 
+{
+	return [NSImage class];
+}
+
++ (BOOL)allowsReverseTransformation
+{
+	return NO;
+}
+
+- (id)transformedValue:(id)value
+{
+	BOOL status = [(NSNumber *)value boolValue];
+	
+	NSImage * statusImage = nil;
+
+    if (status) 
+    {
+        statusImage = [NSImage imageNamed:@"autojoin.png"];
+    }
+    
+	return statusImage;
+}
+
+@end
